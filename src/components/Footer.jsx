397fc186a5031e5f43f66564d4b11cc27ccd234d
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2023-05-21 13:25:57 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = (props) => {
           href="https://github.com/jbrandinger"
           aria-label="My GitHub"
         > <span className="badge bg-dark">
-            Hashir Shoaib
+            Joel Brandinger
           </span>
         </a>{" "}
         using <i className="fab fa-react" />
