a04484cf69132ce7c33db7e90208202685554c62
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2023-05-22 10:08:46 -0400

    remove comments and clean code

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -31,19 +31,6 @@ const mainBody = {
 };
 
 // ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-
 const about = {
   show: true,
   heading: "About Me",
@@ -55,14 +42,11 @@ const about = {
 };
 
 // PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
+// Set project length or display specfic projects i.e ["repos-1", "repo-2"]
 const repos = {
   show: false,
   heading: "Recent Projects",
-  gitHubUsername: "jbrandinger", //i.e."johnDoe12Gh"
+  gitHubUsername: "jbrandinger",
   reposLength: 4,
   specificRepos: [],
 };
@@ -72,22 +56,22 @@ const leadership = {
   show: false,
   heading: "Leadership",
   message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+    "Talk about leadership roles and opportunities",
   images: [
     { 
       img: require("../editable-stuff/joel.jpg"), 
       label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      paragraph: "Talk about leadership 1" 
     },
     { 
       img: require("../editable-stuff/joel.jpg"), 
       label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      paragraph: "Talk about leadership 2" 
     },
   ],
   imageSize: {
-    width:"615",
-    height:"450"
+    width:"550",
+    height:"550"
   }
 };
 
@@ -122,7 +106,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, have any questions, or just want to say hi, please feel free to email me at",
+    "I'm currently looking for full-time Data Science or Software Engineering roles! If you know of any positions available, have any questions, or just want to say hi, please feel free to email me at",
   email: "jbrandinger99@gmail.com",
 };
 
@@ -136,16 +120,11 @@ const experiences = {
       date: 'May 2023 – Present',
     },
     {
-      role: 'Data Analytics Intern',// Here Add Company Name
+      role: 'Data Analytics Intern',
       companylogo: require('../assets/img/php.png'),
       date: 'May 2022 – August 2022',
     },
   ]
 }
-
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
