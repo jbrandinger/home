af7db77520ed47fc531080c7c1fc9afc92612a8d
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2025-05-18 09:16:58 -0400

    update get in touch

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -97,7 +97,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I am currently looking for full-time data science and software engineering roles! If you know of any available positions, have any questions, or just want to say hi, please feel free to email me at",
+    "If you have any questions or just want to say hi, please feel free to email me at",
   email: "jbrandinger99@gmail.com",
 };
 
